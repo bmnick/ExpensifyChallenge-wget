6580aac09b3c602936d4fb3a3170cc56abb7b2ae
Ben Nicholas <ben@ben-nicholas.com>
2011-11-17 13:35:33 -0800

    non-fetching file writing

--- mywget/main.cpp
+++ mywget/main.cpp
@@ -7,12 +7,45 @@
 //
 
 #include <iostream>
+#include <string>
+#include <fstream>
+
+void usage(void);
+std::string get_url(const char* url);
+bool write_file(std::string outfile_name, std::string contents);
 
 int main (int argc, const char * argv[])
 {
+    if (argc < 2) {
+        usage();
+        return 1;
+    }
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::string site_contents = get_url(argv[1]);
+    
+    write_file("wget.out", site_contents);
+    
     return 0;
 }
 
+void usage(void) {
+    std::cout << "Usage: mywget url" << std::endl;
+}
+
+std::string get_url(const char* url) {
+    return "Hello there\n";
+}
+
+bool write_file(std::string outfile_name, std::string contents) {
+    std::ofstream out_file(outfile_name.c_str());
+    
+    if (out_file.is_open()) {
+        out_file << contents;
+    } else {
+        return false;
+    }
+    
+    out_file.close();
+    
+    return true;
+}
