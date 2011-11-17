d87b356667bb756a3d015de62a43096af679fe84
Ben Nicholas <ben@ben-nicholas.com>
2011-11-17 13:19:43 -0800

    Initial Commit

---
+++ mywget/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  mywget
+//
+//  Created by Ben Nicholas on 11/17/11.
+//  Copyright (c) 2011 Oracle Corporation. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
